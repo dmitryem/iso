aede1054c0b722e00a9b66e09a5d10668fe7c2f9
aliashevich <Andrei.Liashevich@vsgcommerce.com>
2018-03-02 18:15:37 +0300

    minor changes

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            FileInputStream fstream = new FileInputStream("p000001_ans.brs");
+            FileInputStream fstream = new FileInputStream("p000001.brs");
             BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
             String strLine;
             int number = 0;
